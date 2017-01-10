462dfa9d64aacfbb74bfaa5bd6684c6081e2ee10
Villeneuve Michaël <contact@michaelvilleneuve.fr>
2017-01-10 11:06:44 +0100

    Removed useless JS, add Three.js

--- gruntfile.js
+++ gruntfile.js
@@ -16,20 +16,6 @@ module.exports = function(grunt) {
         dest: 'public/assets/build/main.js'
       }
     },
-    sass: {
-      dist: {
-        options: {
-          style: 'expanded'
-        },
-        files: [{
-          "expand": true,
-          "cwd": "public/assets/sass/",
-          "src": ["*.sass"],
-          "dest": "public/assets/build/",
-          "ext": ".css"
-        }]
-      }
-    }, 
     watch: {
       scripts: {
         files: ['public/assets/js/*.js', ],
@@ -37,11 +23,7 @@ module.exports = function(grunt) {
         options: {
           spawn: false,
         },
-      },
-      css: {
-        files: 'public/assets/sass/*.sass',
-        tasks: ['sass']
-      },
+      }
     },
   });
 
