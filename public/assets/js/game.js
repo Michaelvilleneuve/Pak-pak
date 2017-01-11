8b67c02fc13925b18d06647649c9c78b0e37a0da
Villeneuve Michaël <contact@michaelvilleneuve.fr>
2017-01-11 15:53:28 +0100

    Add global behavior

--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -1,103 +1,128 @@
 var Game = {
-	ennemies: [],
-	textures: [],
-	init: function() {
-		this.renderer = PIXI.autoDetectRenderer(512, 512);
-		
-		document.querySelector('.game').appendChild(this.renderer.view);
+    enemies: [],
+    columns_nb: 49,
 
-		this.stage = new PIXI.Container();
-		this.renderer.render(this.stage);
+    init: function() {
+        this.setPlate();
+        this.setEnemies();
+        MainChar.init();
+        this.setEvents();
+    },
 
-		this.setColor();
-		this.setup();
-	},
+    setPlate: function() {
+        let y = 0;
+        let x = 0;
+        for(var i = 0; i < Game.columns_nb; i++) {
+            if(i%7 === 0) y++;
+            x = i%7+1;
+            $('#game').append("<div data-x='"+x+"' data-y='"+y+"' id='case-"+(i+1)+"' class='case'></div>");
+        }
+    },
 
-	setColor: function() {
-		this.renderer.backgroundColor = 0x061639;
-		this.renderer.render(this.stage);
-	},
+    setEvents: function() {
+        $(document).on('click', '.case', function() {
+            Game.update($(this));
+        });
+        $('.case').droppable({
+            accept: function(el) {
+                if(MainChar.isOnSameLine($(this).data('x'), $(this).data('y'))) {
+                    return true;
+                }
+            },
+            drop: function(event, ui) {
+                MainChar.eat(event.target);
+            },
+        });
+    },
 
-	setup: function() {
-		this.setupTextures();
-		this.setupEnemies();
+    setEnemies: function() {
+        for(i = 0; i < 48; i++) {
+            const enemy_number = Math.floor(Math.random() * 5) + 1;
+            this.enemies.push(new Enemy(i+1, enemy_number));
+        }
+        this.showEnemies();
+    },
 
-	},
+    showEnemies: function() {
+        for(let i = 0; i < this.enemies.length; i++) {
+            const rotate = ((Math.random() >= 0.5) ? "rotate":"");
+            $('#case-'+this.enemies[i].case_id).append("\
+                <img data-id='"+i+"' class='"+rotate+"' src='"+this.enemies[i].image()+"'>\
+            ");
+        }
+    },
 
-	setupEnemies: function() {
-		this.ennemiesAssets = [
-			"assets/img/ennemy1.png",
-			"assets/img/ennemy2.png",
-			"assets/img/ennemy3.png",
-			"assets/img/ennemy4.png"
-		];
 
-		for(var i = 0; i < this.ennemiesAssets.length; i++) {
-			PIXI.loader
-			  .add(this.ennemiesAssets[i])
-		  	  .load(Enemy.setup.bind(this, i));
-			
-		}
-	},
+    update: function(a_case) {
+        console.log(a_case);
+    },
 
-	setupTextures: function() {
-		this.texturesAssets = [
-			"assets/img/plateau.png",
-		];
+    setControls: function() {
 
-		for(var i = 0; i < this.texturesAssets.length; i++) {
-			PIXI.loader
-			  .add(this.texturesAssets[i])
-		  	  .load(Textures.setup.bind(this, i));
-			
-		}
-	}
-}
-
-Textures = {
-	setup: function(i) {
-		this.textures.push(
-			new PIXI.Sprite(
-				PIXI.loader.resources[
-					this.texturesAssets[i]
-				].texture
-			)
-		);
-
-		let texture = this.textures[this.textures.length - 1];
-		texture.x = 100;
-		texture.y = 140;
-		texture.width = 320;
-		texture.height = 280;
-
-		console.log(texture);
-		this.stage.addChild(texture);
-		this.renderer.render(this.stage);
-	}
+    }
 }
 
 
-Enemy = {
-	setup: function(i) {
-		this.ennemies.push(
-			new PIXI.Sprite(
-				PIXI.loader.resources[
-					this.ennemiesAssets[i]
-				].texture
-			)
-		);
+MainChar = {
+    init: function() {
+        $(".case:not(:has(>img))").append("<img id='main-char' src='assets/img/personnageprincipal.png'>");
+        this.updatePosition($('#main-char').parent('div'));
+        $('#main-char').draggable({containment: "#game",revert: 'invalid'});
+    },
 
-		let lastEnemy = this.ennemies[this.ennemies.length - 1];
+    eat: function(div) {
+        let id = $(div).find('img').data('id');
+        let x = $(div).data('x');
+        let y = $(div).data('y');
 
-		lastEnemy.x = Math.floor(Math.random() * 200) + 140;
-		lastEnemy.y = Math.floor(Math.random() * 200) + 150;
+        if(id && MainChar.isOnSameLine(x, y)) {
+            $(div).find('img').remove();
+        }
+        this.updatePosition(div);
+        return MainChar.isOnSameLine(x, y); 
+    },
 
-		lastEnemy.width = 30;
-		lastEnemy.height = 30;
+    isOnSameLine(x, y) {
+        return $('#main-char').data('x') === x || $('#main-char').data('y') === y; 
+    },
 
-		this.stage.addChild(lastEnemy);
-		this.renderer.render(this.stage);
-	},
+    updatePosition: function(div) {
+        $('#main-char').data('x', $(div).data('x')); 
+        $('#main-char').data('y', $(div).data('y')); 
+    }
+}
+
+class Enemy {
+    constructor(case_id, type) {
+        this.case_id = case_id;
+        this.type = type;
+    }
+
+    points() {
+        let points = 0;
+        switch(this.type) {
+            case 1:
+                points = 10;
+            break;
+            case 2:
+                points = 20;
+            break;
+            case 3:
+                points = 30;
+            break;
+            case 4:
+                points = 40;
+            break;
+            case 5:
+                points = 50;
+            break;
+        }
+        return points;
+    }
+
+    image() {
+        return "assets/img/ennemy" + this.type + ".png";
+    }
 }
 
 
