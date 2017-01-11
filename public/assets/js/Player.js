6dbb6118db9024c8df786b0b54183041370cc9a5
Diez Gabriel <diez.gabriel64@gmail.com>
2017-01-11 11:26:51 +0100

    add UpdateScore method in gameController

--- public/assets/js/Player.js
+++ public/assets/js/Player.js
@@ -1,10 +1,16 @@
 class Player {
-  constructor(name) {
+  constructor(name, id) {
+    this.id = id;
     this.name = name;
     this.round = 0;
+    this.score = 0;
   }
 
   addRound() {
     this.round = this.round + 1;
+  }
+
+  addPoints(points) {
+    this.score += points;
   }
 };
