3a0049b48b4c5ba01ca5ded21c797b4b8f0bc91c
oRiamn <rdurade@etu.u-bordeaux.fr>
2017-01-12 11:28:48 +0100

    Init unit tests

---
+++ spec/Player.test.js
@@ -0,0 +1,15 @@
+require("../public/assets/js/Player.js");
+
+describe("Joueur", function() {
+    describe("Les points du joueur", function() {
+
+    	
+       /* Tuto 
+        it("Devrait multiplier le nombre de livre par 2.2", function() {
+            expect(Conversion.livreVersKilo(22)).toBe(10);
+        });
+
+        */
+                
+    });
+});
