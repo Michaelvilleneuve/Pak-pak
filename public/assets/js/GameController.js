2b60ab0a4f993c366142b0f8c18f56c36a1cf7fb
Diez Gabriel <diez.gabriel64@gmail.com>
2017-01-12 10:49:17 +0100

    remove nextRound from gameController

--- public/assets/js/GameController.js
+++ public/assets/js/GameController.js
@@ -1,14 +1,5 @@
 // Class
 class GameController {
-  nextRound() {
-    if (p2.round < p1.round) {
-      p2.addRound();
-      // @TODO Add css on player for identify
-    } else {
-      p1.addRound();
-      // @TODO Add css on player for identify
-    }
-  }
 
   updateScore(player, points) {
     player.addPoints(points);
